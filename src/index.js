43d55458be7637f231783e3166bde339fef323a1
fjoralba29 <90421671+fjoralba29@users.noreply.github.com>
2022-11-30 16:18:22 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,29 +1,48 @@
 import fsp from 'fs/promises'; 
-import http from 'http';
+import http, { request } from 'http';
+import express, { response } from 'express'; 
 
-const server = http.createServer(async (request, respond) => {
-    if (request.url === '/') {
-        const content = await fsp.readFile('./index.html', 'utf8');
-        respond.statusCode = 200;
-        respond.setHeader('Content-Type', 'text/html');
-        respond.end(content);
-    }
-    else if (request.url === '/about') {
-        const content = await fsp.readFile('./about.html', 'utf8');
-        respond.statusCode = 200;
-        respond.setHeader('Content-Type', 'text/html');
-        respond.end(content);
-    }
-    else {
-        respond.statusCode = 404;
-        respond.setHeader('Content-Type', 'text/plain');
-        respond.end('Not found!');
-    }
-})
+const app = express();
 
-const hostname = '127.0.0.1';
-const port = 3000;
+app.get('/', async (request, response) => {
+    const content = await fsp.readFile('./index.html', 'utf8');
+    respond.send(content);
+});
 
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`)
-  });
+app.get('/about', async (request, response) => {
+    const content = await fsp.readFile('./about.html', 'utf8');
+    respond.send(content);
+});
+
+const port = 3000; 
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`)
+});
+
+
+// const server = http.createServer(async (request, respond) => {
+//     if (request.url === '/') {
+//         const content = await fsp.readFile('./index.html', 'utf8');
+//         respond.statusCode = 200;
+//         respond.setHeader('Content-Type', 'text/html');
+//         respond.end(content);
+//     }
+//     else if (request.url === '/about') {
+//         const content = await fsp.readFile('./about.html', 'utf8');
+//         respond.statusCode = 200;
+//         respond.setHeader('Content-Type', 'text/html');
+//         respond.end(content);
+//     }
+//     else {
+//         respond.statusCode = 404;
+//         respond.setHeader('Content-Type', 'text/plain');
+//         respond.end('Not found!');
+//     }
+// })
+
+// const hostname = '127.0.0.1';
+// const port = 3000;
+
+// server.listen(port, hostname, () => {
+//     console.log(`Server running at http://${hostname}:${port}/`)
+//   });
