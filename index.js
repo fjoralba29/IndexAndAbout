7f876575d5c6319dc72ae84a89ee24c7fe3399a5
fjoralba29 <90421671+fjoralba29@users.noreply.github.com>
2022-11-30 15:24:55 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,29 @@
+import fsp from 'fs/promises'; 
+import http from 'http';
+
+const server = http.createServer(async (request, respond) => {
+    if (request.url === '/') {
+        const content = await fsp.readFile('./index.html', 'utf8');
+        respond.statusCode = 200;
+        respond.setHeader('Content-Type', 'text/plain');
+        respond.end(content);
+    }
+    else if (request.url === '/about') {
+        const content = await fsp.readFile('./about.html', 'utf8');
+        respond.statusCode = 200;
+        respond.setHeader('Content-Type', 'text/plain');
+        respond.end(content);
+    }
+    else {
+        respond.statusCode = 404;
+        respond.setHeader('Content-Type', 'text/plain');
+        respond.end('Not found!');
+    }
+})
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`)
+  });
